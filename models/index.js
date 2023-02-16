b47c77b751f26fbd61d7e87354c71b02753c29fa
Erv <marcnicolas0915@gmail.com>
2023-02-16 13:57:21 -0500

    model bug

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  as: 'commenter',
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comment };
